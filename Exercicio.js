9e5603dd7e58c14149f19623e9e3a16574913290
Douglas Freitas <freitas28douglas@gmail.com>
2022-04-10 21:49:11 -0300

    Update Exercicio.js

--- Exercicio.js
+++ Exercicio.js
@@ -3,10 +3,9 @@ var MeuArray = [1,3,5,10]
 function SomaArray(arg){
     let soma = 0;
 
-    for (let i = 0; i < MeuArray.length; i++){
-        soma += MeuArray[i];
+    for (let i = 0; i < arg.length; i++){
+        soma += arg[i];
     }
     return soma;
 }
-console.log(SomaArray())
-
+console.log(SomaArray(MeuArray))
